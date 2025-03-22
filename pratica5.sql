25a9a4d20258d82ae1cef1af3f200be2c83ddac7
guilherme garcia <143908779+guigvrcia@users.noreply.github.com>
2025-03-22 20:26:34 -0300

    Update pratica5.sql

--- pratica5.sql
+++ pratica5.sql
@@ -10,9 +10,9 @@ SET search_path TO universidade;
 -- Tabela FACULDADE
 -- -----------------------------------------------------
 CREATE TABLE faculdade (
-  sigla           CHAR(5)           NOT NULL,
-  nome            VARCHAR(100),
-  predio    CHAR(5),
+  sigla CHAR(5) NOT NULL,
+  nome VARCHAR(100),
+  predio CHAR(5),
   orcamento DECIMAL(10,2),
   -- restrições
   CONSTRAINT pk_faculdade PRIMARY KEY (sigla)
@@ -22,10 +22,10 @@ CREATE TABLE faculdade (
 -- Tabela PROFESSOR
 -- -----------------------------------------------------
 CREATE TABLE professor (
-  id        CHAR(11)    NOT NULL,
-  nome            VARCHAR(255)      NOT NULL,
-  fac_prof  CHAR(5)           NOT NULL,
-  admissao  DATE,
+  id CHAR(11) NOT NULL,
+  nome VARCHAR(255) NOT NULL,
+  fac_prof CHAR(5) NOT NULL,
+  admissao DATE,
   -- restrições
   CONSTRAINT pk_professor PRIMARY KEY (id),
   CONSTRAINT fk_faculdade FOREIGN KEY (fac_prof) REFERENCES faculdade (sigla)
@@ -35,12 +35,12 @@ CREATE TABLE professor (
 -- Tabela ESTUDANTE
 -- -----------------------------------------------------
 CREATE TABLE estudante (
-  id        CHAR(11)    NOT NULL,
-  nome            VARCHAR(255)      NOT NULL,
-  datanasc  DATE,
-  fac_est   CHAR(5)           NOT NULL,
-  cra       REAL,
-  tutor           CHAR(11),
+  id CHAR(11) NOT NULL,
+  nome VARCHAR(255) NOT NULL,
+  datanasc DATE,
+  fac_est CHAR(5) NOT NULL,
+  cra REAL,
+  tutor CHAR(11),
   -- restrições
   CONSTRAINT pk_estudante PRIMARY KEY (id),
   CONSTRAINT fk_faculdade FOREIGN KEY (fac_est) REFERENCES faculdade (sigla),
@@ -51,10 +51,10 @@ CREATE TABLE estudante (
 -- Tabela DISCIPLINA
 -- -----------------------------------------------------
 CREATE TABLE disciplina (
-  codigo    CHAR(6)           NOT NULL,
-  nome            VARCHAR(70) NOT NULL,
-  fac_disc  CHAR(5)           NOT NULL,
-  ch      SMALLINT, -- carga horária
+  codigo CHAR(6) NOT NULL,
+  nome VARCHAR(70) NOT NULL,
+  fac_disc CHAR(5) NOT NULL,
+  ch SMALLINT, -- carga horária
   -- restrições
   CONSTRAINT pk_disciplina PRIMARY KEY (codigo),
   CONSTRAINT fk_faculdade FOREIGN KEY (fac_disc) REFERENCES faculdade (sigla)
@@ -64,8 +64,8 @@ CREATE TABLE disciplina (
 -- Tabela PRE-REQUISITO
 -- -----------------------------------------------------
 CREATE TABLE pre_requisito (
-  cod_disc  CHAR(6)     NOT NULL,
-  cod_pre   CHAR(6)     NOT NULL,
+  cod_disc CHAR(6) NOT NULL,
+  cod_pre CHAR(6) NOT NULL,
   -- restrições
   CONSTRAINT pk_pre_requisito PRIMARY KEY (cod_disc, cod_pre),
   CONSTRAINT fk_disciplina FOREIGN KEY (cod_disc) REFERENCES disciplina (codigo),
@@ -76,9 +76,9 @@ CREATE TABLE pre_requisito (
 -- Tabela SALA
 -- -----------------------------------------------------
 CREATE TABLE sala (
-  predio    CHAR(5)           NOT NULL,
-  numero    SMALLINT          NOT NULL,
-  capacidade      INTEGER,
+  predio CHAR(5) NOT NULL,
+  numero SMALLINT NOT NULL,
+  capacidade INTEGER,
   -- restrições
   CONSTRAINT pk_sala PRIMARY KEY (predio, numero)
 );
@@ -87,13 +87,13 @@ CREATE TABLE sala (
 -- Tabela TURMA
 -- -----------------------------------------------------
 CREATE TABLE turma (
-  id        INTEGER     NOT NULL,
-  turma           CHAR(2)           NOT NULL,
-  semestre  INTEGER           NOT NULL,
-  ano       INTEGER           NOT NULL,
-  cod_disc  CHAR(6)           NOT NULL,
-  predio_s  CHAR(5),
-  n_sala    INTEGER,
+  id INTEGER NOT NULL,
+  turma CHAR(2) NOT NULL,
+  semestre INTEGER NOT NULL,
+  ano INTEGER NOT NULL,
+  cod_disc CHAR(6) NOT NULL,
+  predio_s CHAR(5),
+  n_sala INTEGER,
   -- restrições
   CONSTRAINT pk_turma PRIMARY KEY (id),
   CONSTRAINT uq_turma UNIQUE (turma, semestre, ano, cod_disc),
@@ -105,8 +105,8 @@ CREATE TABLE turma (
 -- Tabela ENSINA
 -- -----------------------------------------------------
 CREATE TABLE ensina (
-  id_prof   CHAR(11)    NOT NULL,
-  id_turma  INTEGER           NOT NULL,
+  id_prof CHAR(11) NOT NULL,
+  id_turma INTEGER NOT NULL,
   -- restrições
   CONSTRAINT pk_ensina PRIMARY KEY (id_prof, id_turma),
   CONSTRAINT fk_prof_ensina FOREIGN KEY (id_prof) REFERENCES professor (id),
@@ -117,9 +117,9 @@ CREATE TABLE ensina (
 -- Tabela FREQUENTA
 -- -----------------------------------------------------
 CREATE TABLE frequenta (
-  id_est    CHAR(11)    NOT NULL,
-  id_turma  INTEGER     NOT NULL,
-  nota      REAL,
+  id_est CHAR(11) NOT NULL,
+  id_turma INTEGER NOT NULL,
+  nota REAL,
   -- restrições
   CONSTRAINT pk_frequenta PRIMARY KEY (id_est, id_turma),
   CONSTRAINT fk_est_frequenta FOREIGN KEY (id_est) REFERENCES estudante (id),
@@ -130,9 +130,9 @@ CREATE TABLE frequenta (
 -- Tabela HORARIO
 -- -----------------------------------------------------
 CREATE TABLE horario (
-  id_hora         CHAR(1)     NOT NULL,   
-  hora_inicio     TIME,
-  hora_fim  TIME,
+  id_hora CHAR(1) NOT NULL,   
+  hora_inicio TIME,
+  hora_fim TIME,
   -- restrições
   CONSTRAINT pk_horario PRIMARY KEY (id_hora)
 );
@@ -142,7 +142,7 @@ CREATE TABLE horario (
 -- Tabela SEMANA
 -- -----------------------------------------------------
 CREATE TABLE semana (
-  id_sem          CHAR(1)     NOT NULL,   
+  id_sem CHAR(1) NOT NULL,   
   descricao VARCHAR(13),
   -- restrições
   CONSTRAINT pk_semana PRIMARY KEY (id_sem)
@@ -153,8 +153,8 @@ CREATE TABLE semana (
 -- -----------------------------------------------------
 CREATE TABLE horario_aula (
   id_sem CHAR(1) NOT NULL,
-  id_hora   CHAR(1)     NOT NULL,
-  id_turma  INTEGER           NOT NULL,
+  id_hora CHAR(1) NOT NULL,
+  id_turma INTEGER NOT NULL,
 
   -- restrições
   CONSTRAINT pk_horario_aula PRIMARY KEY (id_sem,id_hora, id_turma),
